7120b3d8f3a95e48087c436c643caca71120048f
PlaywithPY <playwithpy@gmail.com>
2025-10-02 23:34:51 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>🚀 Test Next.js Réussi !</h1>
+      <p>Si tu vois ce message, Next.js fonctionne parfaitement !</p>
+    </div>
+  )
+}
